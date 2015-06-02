6854716271633d9b91d85b344ae6c921e779efb1
gahara <gahara@MacBook-Pro-gahara.local>
2015-06-02 21:15:43 +0300

    lab 4 edited

--- tpr4.py
+++ tpr4.py
@@ -8,7 +8,7 @@ class Bunch(object):
         return repr(self.__dict__)
 
 class Tank(object):
-    spa = 0.1 #speed per acceleration
+    fpg = 0.1 #speed per gun
     sps = 1.0 #ster per speed
     max_h = 1.0 #max health
     min_h = 0.01 #min health
@@ -30,7 +30,7 @@ class Tank(object):
             'nearest': lambda _, competitors: sorted(competitors, key=lambda r: r.dist),
             'highest_max_speed': lambda _, competitors: sorted(competitors, key=lambda r: r.tank.max_speed, reverse=True),
             'highest_cur_speed': lambda _, competitors: sorted(competitors, key=lambda r: r.tank.cur_speed, reverse=True),
-            'highest_acceleration': lambda _, competitors: sorted(competitors, key=lambda r: r.tank.acceleration, reverse=True),
+            'gun_power': lambda _, competitors: sorted(competitors, key=lambda r: r.tank.gun, reverse=True),
     }
 
     def __init__(self, **kwargs):
@@ -40,11 +40,14 @@ class Tank(object):
 
     def damage(self):
         return self.strength * self.cur_speed * self.health * self.control
+    
     def endure(self):
         return self.durability * self.cur_speed * self.health * self.control
+   
     def sa_prob(self, competitor):
         my_damage, competitior_endurance = self.damage(), competitor.tank.endure()
-        return (1 - competitor.dist / self.max_ad) * ((my_damage - competitior_endurance) / (my_damage + competitior_endurance + 1e-5))
+        return (1 - competitor.dist / self.max_ad) * ((my_damage - competitior_endurance) / (my_damage + competitior_endurance + 1e-6))
+    
     def attack(self, competitor):
         sucess_probability = self.sa_prob(competitor)
         if sucess_probability <= self.min_sa_p:
@@ -75,15 +78,15 @@ class Tank(object):
     def is_alive(self):
         return self.health > self.min_h
 
-    def make_move_step(self, is_turn):
+    def make_move_step(self, is_rough):
         if not self.is_alive():
             return 0
 
-        if is_turn:
+        if is_rough:
             self.cur_speed *= self.control / self.ctrl
         elif self.cur_speed < self.max_speed:
             old_speed = self.cur_speed
-            self.cur_speed = min(self.max_speed, self.cur_speed + self.acceleration * self.spa)
+            self.cur_speed = min(self.max_speed, self.cur_speed + self.gun * self.fpg)
         return self.cur_speed * self.sps
 
     def make_attack_step(self, competitors):
@@ -95,7 +98,7 @@ class Tank(object):
             if not self.attack(competitor):
                 break
 
-    def state_to_str(self):
+    def make_str(self):
         if not self.is_alive():
             return 'Blown up'
         return 'speed={:.2f}, health={:.2f}, damage={:.2f}, endurance={:.2f}'.format(
@@ -104,20 +107,19 @@ class Tank(object):
         return 'Tank(name={}, cur_sp={:.2f}, h={:.2f})'.format(self.name, self.cur_speed, self.health)
 
 class Battle(object):
-    TURNS_DENSITY = 0.1
+    rough_road = 0.1
 
     def __init__(self, tanks, track_len):
         self._tanks = tanks
         random.shuffle(self._tanks)
         self._track_len = track_len
         self._tank_positions = [0 for _ in range(track_len)]
+    
     def make_step(self):
-        is_turn = random.random() < self.TURNS_DENSITY
-        if is_turn:
-            print('turn on the track!')
+        is_rough = random.random() < self.rough_road
 
         for i, tank in enumerate(self._tanks):
-            self._tank_positions[i] += tank.make_move_step(is_turn)
+            self._tank_positions[i] += tank.make_move_step(is_rough)
 
         for i, tank in enumerate(self._tanks):
             distances = map(lambda p: abs(self._tank_positions[i] - p), self._tank_positions)
@@ -125,24 +127,25 @@ class Battle(object):
             tank.make_attack_step(competitors)
 
         return all(pos < self._track_len for pos in self._tank_positions) and any(tank.is_alive() for tank in self._tanks)
-    def get_winner(self):
+    
+    def calc_winner(self):
         return max(zip(self._tanks, self._tank_positions), key=lambda v: v[1])[0]
+    
     def print_state(self):
         for i, (tank, pos) in enumerate(sorted(zip(self._tanks, self._tank_positions), key=lambda v: v[1], reverse=True)):
-            print('{}: {:.1f}/{} passed, {}: {}'.format(i + 1, min(self._track_len, pos), self._track_len, tank.name, tank.state_to_str()))
+            print('{}: {:.1f}/{} covered, {}: {}'.format(i + 1, min(self._track_len, pos), self._track_len, tank.name, tank.make_str()))
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) == 2:
         random.seed(int(sys.argv[1]))
     tanks = [
-#        Tank(name='Jaguar C-X75', max_speed=8.6, acceleration=8.5, control=8.3, strength=4.2, durability=3.8),
-        Tank(name='T34', max_speed=8.4, acceleration=8.1, control=4.8, strength=2.3, durability=3.6, at_strategy='nearest'),
-        Tank(name='T72', max_speed=6.1, acceleration=6.9, control=5.7, strength=7.4, durability=4.0, at_strategy='highest_acceleration'),
-        Tank(name='Matilda', max_speed=5.6, acceleration=7.8, control=6.2, strength=7.3, durability=4.8, at_strategy='highest_max_speed'),
-        Tank(name='Sherman', max_speed=6.0, acceleration=5.8, control=8.0, strength=6.4, durability=7.8, at_strategy='highest_max_speed'),
-        Tank(name='Hellcat', max_speed=6.7, acceleration=6.6, control=6.0, strength=3.4, durability=3.0, at_strategy='highest_cur_speed'),
-        Tank(name='Vickers', max_speed=4.2, acceleration=8.0, control=3.9, strength=6.7, durability=4.2, at_strategy='highest_acceleration'),
+        Tank(name='T34', max_speed=7.9, gun=8.7, control=3.8, strength=3.3, durability=4.6, at_strategy='nearest'),
+        Tank(name='T72', max_speed=6.1, gun=6.9, control=5.7, strength=7.4, durability=4.0, at_strategy='gun_power'),
+        Tank(name='Matilda', max_speed=5.6, gun=7.8, control=6.2, strength=7.3, durability=4.8, at_strategy='highest_max_speed'),
+        Tank(name='Sherman', max_speed=6.0, gun=5.8, control=8.0, strength=6.4, durability=7.8, at_strategy='highest_max_speed'),
+        Tank(name='Hellcat', max_speed=6.7, gun=6.6, control=6.0, strength=3.4, durability=3.0, at_strategy='highest_cur_speed'),
+        Tank(name='Vickers', max_speed=4.2, gun=8.0, control=3.9, strength=6.7, durability=4.2, at_strategy='gun_power'),
     ]
 
     battle = Battle(tanks=tanks, track_len=100)
@@ -157,6 +160,4 @@ if __name__ == '__main__':
         r = battle.make_step()
         battle.print_state()
         print('')
-    print('Winner: {}'.format(battle.get_winner().name))
-    import os
-    os.system('say "Tank {} won the battle!"'.format(battle.get_winner().name))
+    print('Winner: {}'.format(battle.calc_winner().name))
